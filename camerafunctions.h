7a8e57d8fd4eff6952e8a90907293fa3f4b63d26
Dust1n <Dust1n@users.noreply.github.com>
2014-10-22 14:00:11 -0500

    Create camerafunctions.h

---
+++ camerafunctions.h
@@ -0,0 +1,10 @@
+#ifndef CAMERAFUNCTIONS
+#define CAMERAFUNCTIONS
+
+enum Color{
+	black=0, white=1, gray=2, pink=3, blue=4, green=5, purple=6, yellow=7, brown=8, orange=9, red=10
+};
+
+void setColorTiles(enum Color t[]); 
+
+#endif
